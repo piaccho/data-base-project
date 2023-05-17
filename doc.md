6da1533ccdb578f8dce098b5558658a7ee13554e
Sebastian Piaskowy <sebi77styp@gmail.com>
2023-05-17 21:02:04 +0200

    docs

--- doc.md
+++ doc.md
@@ -1,3 +1,192 @@
 # Dokumentacja
-Backend naszego projektu oparty jest o serwer w technologii `Node.js` z frameworkiem `Express.js` oraz systemem bazy danych w technologii `MongoDB` 
+Backend naszego projektu oparty jest o serwer w technologii `Node.js` z frameworkiem `Express.js` oraz bazą danych w technologii `MongoDB` 
 
+
+## Schemat bazy danych
+### Specyfikacja kolekcji
+
+#### 1. ***customers*** (Klienci):
+
+Opis:
+- `_id` - identyfikator klienta
+- `firstname` - imię klienta 
+- `lastname` - nazwisko klienta
+- `email` - adres e-mail klienta
+- `phone` - nr telefonu klienta
+- `address` - adres klienta
+- `login` - login klienta
+- `password` - hasło klienta (haszowane przy użyciu biblioteki bcryptjs)
+- `registered` - data rejestracji konta klienta
+
+
+Przykładowy dokument:
+
+```json
+{
+    "_id": "64651fcc334f855355b22e79",
+    "firstname": "John",
+    "lastname": "Smith",
+    "email": "jsmith@gmail.com",
+    "phone": "+ ",
+    "address": " ",
+    "login": "jsmith123",
+    "password": "$2a$10$W76xT6.M1Qm8X89gP1HAN.WJJ/LHCRFkma6O7R0YZfAOMzmZYWqPi",
+    "registerDate": "2023-05-16 20:27:21"
+}
+```
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 2. ***suppliers*** (Dostawcy):
+
+Opis:
+- `_id` - ObjectId
+- `companyName` - String
+- `email` - String
+- `city` - String
+- `street` - String
+- `postalCode` - String
+- `phone` - String
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 3. ***categories*** (Kategorie):
+
+Opis:
+- `_id` - ObjectId
+- `name` - String
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 4. ***products*** (Produkty):
+
+Opis:
+- `_id` - ObjectId
+- `name` - String
+- `description` - String
+- `price` - Number
+- `category` - ObjectId (referencja do Kategorii)
+- `image` - String (ścieżka do obrazka)
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 5. ***carts*** (Koszyki):
+
+Opis:
+- `_id` - ObjectId
+- `user` - ObjectId (referencja do Użytkownika)
+- `products` - Array of Objects
+- `product` - ObjectId (referencja do Produktu)
+- `quantity` - Number
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 6. ***orders*** (Zamówienia):
+
+Opis:
+- `_id` - ObjectId
+- `user` - ObjectId (referencja do Użytkownika)
+- `products` - Array of Objects
+- `product` - ObjectId (referencja do Produktu)
+- `quantity` - Number
+- `total` - Number
+- `address` - String
+- `status` - String (np. "pending", "shipped", "delivered")
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 7. ***reviews*** (Recenzje):
+
+Opis:
+- `_id` - ObjectId
+- `user` - ObjectId (referencja do Użytkownika)
+- `product` - ObjectId (referencja do Produktu)
+- `rating` - Number (ocena produktu)
+- `comment` - String (komentarz)
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 8. ***wishlists*** (Listy życzeń):
+
+Opis:
+- `_id` - ObjectId
+- `user` - ObjectId (referencja do Użytkownika)
+- `products` - Array of ObjectId (referencje do Produktów)
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 9. ***payments*** (Płatności):
+
+Opis:
+- `_id` - ObjectId
+- `user` - ObjectId (referencja do Użytkownika)
+- `order` - ObjectId (referencja do Zamówienia)
+- `amount` - Number (kwota płatności)
+- `paymentMethod` - String (metoda płatności, np. "karta kredytowa", "przelew bankowy")
+- `status` - String (status płatności, np. "sukces", "odrzucono", "w trakcie")
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 10. ***promotions*** (Promocje):
+
+Opis:
+- `_id` - ObjectId
+- `name` - String (nazwa promocji)
+- `description` - String (opis promocji)
+- `startDate` - Date (data rozpoczęcia promocji)
+- `endDate` - Date (data zakończenia promocji)
+- `products` - Array of ObjectId (referencje do Produktów objętych promocją)
+
+Przykładowy dokument
+```json
+{
+    
+}
+```
+#### 11. ***shipping*** (Wysyłka):
+
+Opis:
+- `_id` - ObjectId
+- `order` - ObjectId (referencja do Zamówienia)
+- `address` - String (adres dostawy)
+- `city` - String (miasto)
+- `country` - String (kraj)
+- `postalCode` - String (kod pocztowy)
+- `trackingNumber` - String (numer śledzenia przesyłki)
+- `status` - String (status wysyłki, np. "w trakcie", "dostarczono")
--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
-import express, { query } from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
-import mongodb from 'mongodb'
 import { fileURLToPath } from 'url';
 
 const port = 8001;
 
-let students = {};
+import mongodb from 'mongodb'
+import mongoose from 'mongoose';
+const MongoClient = mongodb.MongoClient;
+const client = new MongoClient('mongodb://127.0.0.1:27017');
+await client.connect(err => {
 
-async function getDbData(department="") {
-    const MongoClient = mongodb.MongoClient;
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-    const db = client.db('AGH');
-    const collection = db.collection('students');
-    const query = department === "" ? {} : {'faculty': department};
-    students = await collection.find(query).toArray();
-    client.close();
-}
+});
+const db = client.db('webshop');
+
+
+// async function getDbData() {
+//     const collection = db.collection('students');
+//     const query = department === "" ? {} : {'faculty': department};
+//     students = await collection.find(query).toArray();
+//     client.close();
+// }
 
 /* *************************** */
 /* Configuring the application */
@@ -26,7 +29,6 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('views', __dirname + '/views'); // Files with views can be found in the 'views' directory
 app.set('view engine', 'pug'); // Use the 'Pug' template system
 app.locals.pretty = app.get('env') === 'development'; // The resulting HTML code will be indented in the development environment
 
@@ -41,29 +43,75 @@ app.use(express.urlencoded({ extended: false })); // for parsing form sent data
 /* "Routes" */
 /* ******** */
 
+// main
 app.get('/', async function (request, response) {
-    // await getDbData();
-    response.render('index', { mode: true, students: students}); // Render the 'index' view
+    response.render('index', { db: db}); // Render the 'index' view
 });
 
-app.get('/submit', function (request, response) {
-    response.set('Content-Type', 'text/plain')
-    response.send(`Hello ${request.query.name}`); // Send a response to the browser
+app.post('/products', async function (request, response) {
+    const collection = db.collection('products');
+    const query = department === "" ? {} : {'faculty': department};
+    students = await collection.find(query).toArray();
+    response.render('index', { db: db}); // Render the 'index' view
 });
 
-app.post('/', function (request, response) {
-    response.set('Content-Type', 'text/plain')
-    response.send(`Hello ${request.body.name}`);
-});
 
-app.get('/query', async function(request, response) {
-    await getDbData(request.query.department);
-    response.render('index', { mode: true, students: students}); 
-})
+// basic CRUDs for all collections
+
+// get all elements from collection
+// app.get('/', async function (request, response) {
+//     await getDbData();
+//     response.render('index', { db: db}); // Render the 'index' view
+// });
+
+// // get a specific element from collection
+// app.get('/', async function (request, response) {
+//     await getDbData();
+//     response.render('index', { db: db}); // Render the 'index' view
+// });
+
+// // add a new element from collection
+// app.get('/', async function (request, response) {
+//     await getDbData();
+//     response.render('index', { db: db}); // Render the 'index' view
+// });
+
+// // update an element from collection
+// app.get('/', async function (request, response) {
+//     await getDbData();
+//     response.render('index', { db: db}); // Render the 'index' view
+// });
+
+// // remove an element from collection
+// app.get('/', async function (request, response) {
+//     await getDbData();
+//     response.render('index', { db: db}); // Render the 'index' view
+// });
+
+
+
+
+// app.get('/submit', function (request, response) {
+//     response.set('Content-Type', 'text/plain')
+//     response.send(`Hello ${request.query.name}`); // Send a response to the browser
+// });
+
+// app.post('/', function (request, response) {
+//     response.set('Content-Type', 'text/plain')
+//     response.send(`Hello ${request.body.name}`);
+// });
+
+// app.get('/query', async function(request, response) {
+//     // await getDbData(request.query.department);
+//     // response.render('index', { mode: true, students: students}); 
+// })
 
 /* ************************************************ */
 
 app.listen(port, function () {
     console.log(`The server was started on port ${port}`);
     console.log('To stop the server, press "CTRL + C"');
+    console.log('================================');
+    console.log('Incoming HTTP requests...');
+    console.log('================================');
 });          
